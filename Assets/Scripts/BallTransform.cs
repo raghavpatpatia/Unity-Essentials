a70fc8acd1cac94ae19020fcf362f8a0b95e717f
Raghav <63911252+raghavpatpatia@users.noreply.github.com>
2022-11-19 21:18:17 +0530

    Essentials

---
+++ Assets/Scripts/BallTransform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTransform : MonoBehaviour
+{
+    public Vector3 scaleChange;
+    public Vector3 rotateChange;
+    public Vector3 positionChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += (scaleChange * Time.deltaTime);
+        transform.localPosition += (positionChange * Time.deltaTime);
+        transform.Rotate(rotateChange * Time.deltaTime);
+    }
+}
